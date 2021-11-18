6c3c8510d71bcc7143070a9f45d50a173dbe0bd2
Sean Cleaver <sean@simego.com>
2021-11-18 15:15:25 +0000

    Fix to only return Table columns.

--- src/Simego.DataSync.Providers.DbSchema/DbInformationSchemaConstants.cs
+++ src/Simego.DataSync.Providers.DbSchema/DbInformationSchemaConstants.cs
@@ -5,7 +5,7 @@
         public const string Q_TABLE_COUNT = "select count(*) from information_schema.tables WHERE table_schema = @p0 AND table_name = @p1";
         public const string Q_COLUMNS_COUNT = "select count(*) from information_schema.columns WHERE table_schema = @p0 AND table_name = @p1";
         public const string Q_COLUMNS = "select * from information_schema.columns";
-        public const string Q_COLUMNS_SQLSERVER = "select *, columnproperty(object_id('[' + table_schema + '].[' + table_name + ']'), [column_name], 'IsIdentity') AS is_identity from information_schema.columns";
+        public const string Q_COLUMNS_SQLSERVER = "select columns.*, columnproperty(object_id('[' + columns.table_schema + '].[' + columns.table_name + ']'), [column_name], 'IsIdentity') AS is_identity from information_schema.columns left join information_schema.views ON views.table_catalog = columns.table_catalog AND views.table_schema = columns.table_schema AND views.table_name = columns.TABLE_NAME where views.table_name IS NULL";
 
         public const string C_TABLE_SCHEMA = "table_schema";
         public const string C_TABLE_NAME = "table_name";
